34e5c8b4e4bcda70b70aa60b80d9a5e390094d5c
TheZuzanka <wlachovska.zuzana@gmail.com>
2022-10-24 21:41:42 +0200

    player motion fixed

--- NAVPH-FILIP/Assets/Scripts/Player.cs
+++ NAVPH-FILIP/Assets/Scripts/Player.cs
@@ -1,35 +1,38 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Vector2 speed;
+    private Rigidbody2D _rigidbody2D;
     void Start()
     {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
+        //nevola sa rovnako ako update
         if (Input.GetKeyDown(KeyCode.D))
         {
-            transform.position = new Vector2(transform.position.x + 1, transform.position.y);
+            _rigidbody2D.velocity = new Vector2(speed.x, _rigidbody2D.velocity.y);
         }
         
-        if (Input.GetKeyDown(KeyCode.A))
+        else if (Input.GetKeyDown(KeyCode.A))
         {
-            transform.position = new Vector2(transform.position.x - 1, transform.position.y);
+            _rigidbody2D.velocity = new Vector2(-speed.x, _rigidbody2D.velocity.y);
         }
         
         if (Input.GetKeyDown(KeyCode.W))
         {
-            transform.position = new Vector2(transform.position.x, transform.position.y + 1);
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, speed.y);
         }
         
-        if (Input.GetKeyDown(KeyCode.S))
+        else if (Input.GetKeyDown(KeyCode.S))
         {
-            transform.position = new Vector2(transform.position.x, transform.position.y - 1);
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, -speed.y);
         }
     }
 }
