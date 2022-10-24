4ca8e35151a0109afe21c0e347093ff309c6016b
TheZuzanka <wlachovska.zuzana@gmail.com>
2022-10-24 18:22:05 +0200

    failed spawning character

--- NAVPH-FILIP/Assets/Scripts/LevelManager.cs
+++ NAVPH-FILIP/Assets/Scripts/LevelManager.cs
@@ -1,11 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    public Player player;
+    public GameObject levelContainer;
+
     void Start()
     {
-        
+        Debug.Log("x = " + levelContainer.transform.position.x);
+        player = Instantiate(player, new Vector2(0, 0), 
+            Quaternion.identity, levelContainer.transform);
     }
 }
