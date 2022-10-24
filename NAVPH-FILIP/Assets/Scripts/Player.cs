f065713f239216ea5aa4664b130c4dab01e9744c
TheZuzanka <wlachovska.zuzana@gmail.com>
2022-10-24 17:28:36 +0200

    simple moves added

--- NAVPH-FILIP/Assets/Scripts/Player.cs
+++ NAVPH-FILIP/Assets/Scripts/Player.cs
@@ -7,12 +7,29 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position = new Vector2(transform.position.x + 1, transform.position.y);
+        }
         
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position = new Vector2(transform.position.x - 1, transform.position.y);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y + 1);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position = new Vector2(transform.position.x, transform.position.y - 1);
+        }
     }
 }
