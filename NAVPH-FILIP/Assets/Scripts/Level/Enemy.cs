55625f4bc5d6f619d2615d84b145270aae7dc376
Filip Chalas <chalas.filip@gmail.com>
2022-11-10 13:40:43 +0100

    enemy can die when playes jumps on his heads, enemy can move - needs fixing

--- NAVPH-FILIP/Assets/Scripts/Level/Enemy.cs
+++ NAVPH-FILIP/Assets/Scripts/Level/Enemy.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Enemy : MonoBehaviour
 {
+
+    public Transform setLeftBoundary;
+    private float leftBoundaryX;
+    public Transform setRightBoundary;
+    private float rightBoundaryX;
+
     public GameObject player;
     public float hostileDistance = 10.0f;
+    public float enemySpeed = 2.0f;
 
     public GameObject FX;
     public float throwForce = 20.0f;
@@ -16,7 +24,21 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        leftBoundaryX = setLeftBoundary.position.x;
+        rightBoundaryX = setRightBoundary.position.x;
+    }
+
+    private void Move()
+    {
+        float moveUnit = enemySpeed * Time.deltaTime;
+        float moveX = Vector2.MoveTowards(transform.position,
+                    player.transform.position, enemySpeed * Time.deltaTime).x;
+        float moveY = transform.position.y;
+
+        if (((moveX + moveUnit) > leftBoundaryX) && ((moveX + moveUnit) < rightBoundaryX))
+        {
+            transform.position = new Vector2(moveX, moveY);
+        }
     }
 
     // Update is called once per frame
@@ -26,17 +48,25 @@ public class Enemy : MonoBehaviour
 
         // Create new FX object if player is in hostileDistance and time set in spawnInterval
         // has passed since previous FX object spawn
-        if ((distanceFromPlayer <= hostileDistance) && (timePassed >= spawnInterval))
+        if (distanceFromPlayer <= hostileDistance)
         {
-            // Set vector that will point FX object towards player
-            Vector3 fxPosition = gameObject.transform.position;
-            Vector3 fromEnemyToPlayer = player.transform.position - fxPosition;
-            fromEnemyToPlayer.Normalize();
+            if ((transform.position.x > leftBoundaryX) && (transform.position.x < rightBoundaryX))
+            {
+                Move();
+            }
 
-            GameObject newFX = Instantiate(FX, fxPosition, Quaternion.identity);
+            if (timePassed >= spawnInterval)
+            {
+                // Set vector that will point FX object towards player
+                Vector3 fxPosition = transform.position;
+                Vector3 fromEnemyToPlayer = player.transform.position - fxPosition;
+                fromEnemyToPlayer.Normalize();
 
-            // fire FX object towards player
-            newFX.GetComponent<Rigidbody2D>().velocity = fromEnemyToPlayer * throwForce;
+                GameObject newFX = Instantiate(FX, fxPosition, Quaternion.identity);
+
+                // fire FX object towards player
+                newFX.GetComponent<Rigidbody2D>().velocity = fromEnemyToPlayer * throwForce;
+            }
         }
 
         if (timePassed >= spawnInterval)
@@ -45,5 +75,13 @@ public class Enemy : MonoBehaviour
         }
 
         timePassed += Time.deltaTime;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
     }
 }
