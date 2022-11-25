3f1d40b71021b46accf8054c1e15ce306d80011b
TheZuzanka <wlachovska.zuzana@gmail.com>
2022-11-25 18:19:09 +0100

    tooltips on click for attribute enabled

--- NAVPH-FILIP/Assets/Scripts/Settings/SettingsController.cs
+++ NAVPH-FILIP/Assets/Scripts/Settings/SettingsController.cs
@@ -1,6 +1,15 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsController : MonoBehaviour
 {
+    public void DisplayTooltip(Button button)
+    {
+        GameObject tooltip = button.transform.Find("Tooltip").gameObject;
+        tooltip.SetActive(true);
+    }
     
+    void Update () {
+        //transform.position = Input.mousePosition + offset;
+    }
 }
