6adc1849d8ff0ae420030808242a6366d50755cb
TheZuzanka <wlachovska.zuzana@gmail.com>
2022-12-07 15:26:17 +0100

    level finish flag prefab created

---
+++ NAVPH-FILIP/Assets/Scripts/Level/Finish.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Player player = other.GetComponent<Player>();
+            //
+            Destroy(gameObject);
+        }
+    }
+}
