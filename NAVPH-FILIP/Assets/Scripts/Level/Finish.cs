256a39c81067de94aadf5962b3c9a83798a86383
TheZuzanka <wlachovska.zuzana@gmail.com>
2022-12-07 15:49:50 +0100

    level finish done

--- NAVPH-FILIP/Assets/Scripts/Level/Finish.cs
+++ NAVPH-FILIP/Assets/Scripts/Level/Finish.cs
@@ -1,13 +1,24 @@
+using System;
 using UnityEngine;
 
 public class Finish : MonoBehaviour
 {
+    [SerializeField] private bool _isFinished;
+    public delegate void IsFinishedDelegate(bool isFinished);
+
+    public IsFinishedDelegate isFinishedDelegate;
+
+    private void Start()
+    {
+        _isFinished = false;
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            Player player = other.GetComponent<Player>();
-            //
+            _isFinished = true;
+            isFinishedDelegate(_isFinished);
             Destroy(gameObject);
         }
     }
---
+++ NAVPH-FILIP/Assets/Scripts/Level/FinishLevelManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLevelManager : MonoBehaviour
+{
+    private Player _player;
+}
