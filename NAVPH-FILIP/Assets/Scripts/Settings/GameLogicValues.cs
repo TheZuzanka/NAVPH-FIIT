f00ef89f2b9009ed503de2f00d718e19475a9e48
TheZuzanka <wlachovska.zuzana@gmail.com>
2022-12-17 16:00:39 +0100

    NonFxMark refactored, time to live for mark move to Game logic values

--- NAVPH-FILIP/Assets/Scripts/Settings/GameLogicValues.cs
+++ NAVPH-FILIP/Assets/Scripts/Settings/GameLogicValues.cs
@@ -5,6 +5,7 @@ public static class GameLogicValues
     public static float CoffeeEffectMultiplier = 1.1f;
     public static int CoffeeTimerValue = 500;
     public static int ShieldTimerValue = 500;
+    public static float MarkExistingTime = 5f;
     
     public static readonly Dictionary<string, float> MarkCoefficients = new()
     {
